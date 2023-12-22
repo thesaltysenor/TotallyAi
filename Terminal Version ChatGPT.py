72994548836e392374dd7a238e5349f6fe99005a
Sam Pigeon <pigeonsam@gmail.com>
2023-12-21 19:09:19 -0600

    actual file redo gpt terminal

---
+++ Terminal Version ChatGPT.py
@@ -0,0 +1,27 @@
+import openai
+
+# Replace 'your-api-key' with your actual OpenAI API key
+openai.api_key = 'my key messed up'
+
+def get_openai_response(prompt_text):
+    try:
+        response = openai.Completion.create(
+          engine="text-davinci-004",
+          prompt=prompt_text,
+          temperature=0.7,
+          max_tokens=150
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        return str(e)
+
+def main():
+    while True:
+        user_input = input("Enter a prompt: ")
+        if user_input.lower() == 'exit':
+            break
+        response = get_openai_response(user_input)
+        print(response)
+
+if __name__ == "__main__":
+    main()
